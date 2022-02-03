fd9c3e689f0498a4404dd78c7504d86d1797b805
David Allen Archer <David.Allen.Archer@Gmail.com>
2022-02-03 00:36:04 -0500

    Added BlueprintImplementableEvents for navigation to the FlipTacToeGameInstance and Implemented those events in BP_GameInstance

--- Source/FlipTacToe/FlipTacToeGameInstance.cpp
+++ Source/FlipTacToe/FlipTacToeGameInstance.cpp
@@ -3,3 +3,10 @@
 
 #include "FlipTacToeGameInstance.h"
 
+void UFlipTacToeGameInstance::LaunchLobby_Implementation(bool IsLanEnabled, const FText &ServerName)
+{
+	P_ServerName = ServerName;
+	ShowLoadingScreen();
+	// TODO CREATE SESSION
+	// https://youtu.be/78XJYBfWXAA?list=PLZlv_N0_O1gYqSlbGQVKsRg6fpxWndZqZ&t=745
+}
--- Source/FlipTacToe/FlipTacToeGameInstance.h
+++ Source/FlipTacToe/FlipTacToeGameInstance.h
@@ -13,5 +13,22 @@ UCLASS()
 class FLIPTACTOE_API UFlipTacToeGameInstance : public UGameInstance
 {
 	GENERATED_BODY()
+private:
+	FText P_ServerName;
 	
+public:
+	UFUNCTION(BlueprintImplementableEvent, Category = "Navigation")
+	void ShowMainMenu();
+	UFUNCTION(BlueprintImplementableEvent, Category = "Navigation")
+	void ShowHostMenu();
+	UFUNCTION(BlueprintImplementableEvent, Category = "Navigation")
+	void ShowServerMenu();
+	UFUNCTION(BlueprintImplementableEvent, Category = "Navigation")
+	void ShowOptionsMenu();
+
+	UFUNCTION(BlueprintImplementableEvent, Category = "Navigation")
+	void ShowLoadingScreen();
+
+	UFUNCTION(BlueprintNativeEvent, Category = "Navigation")
+	void LaunchLobby(bool IsLanEnabled, const FText &ServerName);
 };
